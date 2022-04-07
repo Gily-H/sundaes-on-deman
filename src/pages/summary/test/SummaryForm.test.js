bb4b2379bda277d91bd682bed2c691bbe7051ab5
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-04-06 23:04:17 -0400

    Test: popover visibility on hover over terms and conditions

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
@@ -16,7 +16,7 @@ test("checkbox is unchecked by default and button is disabled by default", () =>
   expect(confirmButton).toBeDisabled();
 });
 
-test("enable button on first checkbox click and disable button on second checkbox click", () => {
+test("enable button on first checkbox click and disable button on second checkbox click", async () => {
   render(<SummaryForm />);
 
   const termsCheckbox = screen.getByRole("checkbox", { name: "I agree to Terms and Conditions" });
@@ -39,4 +39,27 @@ test("enable button on first checkbox click and disable button on second checkbo
 
   // button should be disabled
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover appears on hover and disappears when not hover", async () => {
+  render(<SummaryForm />);
+
+  // popover should be hidden by default
+  const nullPopover = screen.queryByText("No ice cream will actually be delivered");
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover should appear when hovering
+  const termsAndConditions = screen.getByText("Terms and Conditions");
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText("No ice cream will actually be delivered");
+  expect(popover).toBeInTheDocument();
+
+  // popover should disappear when not hovering
+  // popover disappearing happens asynchronously - tests will finish before popover disappears
+  // need async await call - wait for popover to disappear before asserting
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText("No ice cream will actually be delivered")
+  );
 });
