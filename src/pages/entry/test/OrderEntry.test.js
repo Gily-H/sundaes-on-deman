6cddc9b412b0e48ba73f09ce1b99358d5bef0e2f
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-04-12 23:57:02 -0400

    Test and code for handling and displaying error messages received from failed server request

---
+++ src/pages/entry/test/OrderEntry.test.js
@@ -0,0 +1,30 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import OrderEntry from "../OrderEntry.js";
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
+
+test("handles error for scoops and topping routes", async () => {
+  // mock request failures that result in a status error 500
+  server.resetHandlers(
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  render(<OrderEntry />);
+
+  await waitFor(async () => {
+    // requests are still asynchronous even though they will send an error
+    const errorAlerts = await screen.findAllByRole("alert", {
+      // bootstrap alert (div) component message is not the default accessible name
+      // will not be found unless manually adding an accessible name to the components
+      name: "An unexpected error occurred. Please try again later.",
+    });
+
+    // should have an array of 2 element alerts
+    expect(errorAlerts).toHaveLength(2); // one for scoops and one for toppings
+  });
+});
