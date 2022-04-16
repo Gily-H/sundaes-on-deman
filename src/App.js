b701478f21dfa1cd72dcb00af2154b1d527885f6
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-04-15 23:59:42 -0400

    import OrderEntry component for manual testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import OrderEntry from "./pages/entry/OrderEntry";
 import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
   return (
     <div className="App">
+      <OrderEntry />
       <SummaryForm />
     </div>
   );
