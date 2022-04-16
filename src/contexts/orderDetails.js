a5cfcc9582bc6f9b51696c5d4b3c5dc15351cc94
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-04-16 13:46:25 -0400

    implement provider context for scoops and toppings order details

---
+++ src/contexts/orderDetails.js
@@ -0,0 +1,66 @@
+import { createContext, useContext, useState, useMemo, useEffect } from "react";
+import { pricePerItem } from "../constants";
+
+// did not provide default value to createContext
+// want to check if null when not inside a Provider
+const OrderDetails = createContext();
+
+// create custom hook to check if we are in a Provider
+export function useOrderDetails() {
+  const context = useContext(OrderDetails);
+
+  if (!context) {
+    throw new Error("useOrderDetails must be used within an OrderDetailsProvider");
+  }
+
+  return context;
+}
+
+function calculateSubtotal(optionType, optionCounts) {
+  let optionCount = 0;
+  for (const count of optionCounts[optionType].values()) {
+    optionCount += count;
+  }
+  return optionCount * pricePerItem[optionType];
+}
+
+export function OrderDetailesProvider(props) {
+  const [optionCounts, setOptionCounts] = useState({
+    scoops: new Map(), // key = option name, value = named option count
+    toppings: new Map(), // key = option name, value = named option count
+  });
+  const [totals, setTotals] = useState({
+    scoops: 0,
+    toppings: 0,
+    grandTotal: 0,
+  });
+
+  useEffect(() => {
+    const scoopsSubtotal = calculateSubtotal("scoops", optionCounts);
+    const toppingsSubtotal = calculateSubtotal("toppings", optionCounts);
+    const grandTotal = scoopsSubtotal + toppingsSubtotal;
+    setTotals({
+      scoops: scoopsSubtotal,
+      toppings: toppingsSubtotal,
+      grandTotal, // shortcut will use variable name as the key
+    });
+  }, [optionCounts]);
+
+  // getter: object containing info on selected scoops and toppings; count, subtotal, total
+  // setter: update options count
+  const value = useMemo(() => {
+    function updateItemCount(itemName, newItemCount, optionType) {
+      const newOptionCounts = { ...optionCounts };
+
+      // update option count for this item with the new value
+      const optionCountsMap = optionCounts[optionType]; // get map for specific option
+      optionCountsMap.set(itemName, parseInt(newItemCount)); // update count of named option
+
+      setOptionCounts(newOptionCounts);
+    }
+
+    return [{ ...optionCounts, totals }, updateItemCount];
+  }, [optionCounts, totals]);
+
+  return <OrderDetails.Provider value={value} {...props} />;
+}
