93e64beb44cebb58360cf8d92209ab767be10861
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-04-19 18:12:32 -0400

    Refactor: OrderDetailsProvider setter function in Memo to create a new Map to prevent mutation of the original optionCounts state when setting the new item count inside of the corresponding Map

--- src/contexts/orderDetails.js
+++ src/contexts/orderDetails.js
@@ -1,11 +1,12 @@
 import { createContext, useContext, useState, useMemo, useEffect } from "react";
 import { pricePerItem } from "../constants";
 
-// did not provide default value to createContext
+// intentionally left out default value for createContext
 // want to check if null when not inside a Provider
 const OrderDetails = createContext();
 
-// create custom hook to check if we are in a Provider
+// create custom hook to check if context wrapped in Provider
+// if wrapped, return context, otherwise throw error
 function useOrderDetails() {
   const context = useContext(OrderDetails);
 
@@ -16,6 +17,9 @@ function useOrderDetails() {
   return context;
 }
 
+// helper function to calculate subtotal based on option quantity selected
+// params: optionType: string
+//         optionCounts: object with maps for each option type
 function calculateSubtotal(optionType, optionCounts) {
   let optionCount = 0;
   for (const count of optionCounts[optionType].values()) {
@@ -26,15 +30,17 @@ function calculateSubtotal(optionType, optionCounts) {
 
 function OrderDetailesProvider(props) {
   const [optionCounts, setOptionCounts] = useState({
-    scoops: new Map(), // key = option name, value = named option count
-    toppings: new Map(), // key = option name, value = named option count
+    scoops: new Map(), // key = specific option name (e.g vanilla), value = count for key option
+    toppings: new Map(), // key = specific option name (e.g M&Ms), value = count for key option
   });
   const [totals, setTotals] = useState({
+    // subtotals and grand total
     scoops: 0,
     toppings: 0,
     grandTotal: 0,
   });
 
+  // update subtotals and grand totals when selected option counts are changed by user
   useEffect(() => {
     const scoopsSubtotal = calculateSubtotal("scoops", optionCounts);
     const toppingsSubtotal = calculateSubtotal("toppings", optionCounts);
@@ -46,23 +52,33 @@ function OrderDetailesProvider(props) {
     });
   }, [optionCounts]);
 
-  // getter: object containing info on selected scoops and toppings; count, subtotal, total
-  // setter: update options count
+  // Provider value memoized
+  // returns: array[2]
+  //          object with option counts, subtotals/totals,
+  //          setter function to update the selected option counts
   const value = useMemo(() => {
+    // setter function: update the option counts
     function updateItemCount(itemName, newItemCount, optionType) {
-      const newOptionCounts = { ...optionCounts };
-
-      // update option count for this item with the new value
-      const optionCountsMap = optionCounts[optionType]; // get map for specific option
-      optionCountsMap.set(itemName, parseInt(newItemCount)); // update count of named option
-
-      setOptionCounts(newOptionCounts);
+      setOptionCounts((prevOptionCounts) => {
+        // create new Map to avoid same reference issues for corresponding option type Map
+        // changes in the new Map will not mutate values in the previous state's Map
+        const newOptionsCountMap = new Map(prevOptionCounts[optionType]);
+        // update option count with the new item count for specific item in the new Map 
+        newOptionsCountMap.set(itemName, parseInt(newItemCount)); // count is type string - convert to int
+        // new count object with previous counts, replace the optionType Map with the newly updated option count Map
+        const newOptionsCount = {
+          ...prevOptionCounts,
+          [optionType]: newOptionsCountMap,
+        };
+        return newOptionsCount; // set the state with the new option count object
+      });
     }
-
     return [{ ...optionCounts, totals }, updateItemCount];
-  }, [optionCounts, totals]);
+  }, [optionCounts, totals]); // memo dependencies - recalculate value when dependencies change
 
+  // value to be used by consumer components nested in the Provider
   return <OrderDetails.Provider value={value} {...props} />;
 }
 
+// export Provider and Context
 export { OrderDetailesProvider, useOrderDetails };
