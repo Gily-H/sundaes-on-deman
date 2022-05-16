566269019a76804b78b813f841ad31536f5ea861
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-05-16 01:39:52 -0400

    Test and Code for grand total calculations when selecting Scoop options and Topping options

--- src/pages/entry/test/totalUpdates.test.js
+++ src/pages/entry/test/totalUpdates.test.js
@@ -1,7 +1,8 @@
 import userEvent from "@testing-library/user-event";
 
-import { render, screen } from "../../../test-utils/testing-library-utils";
+import { render, screen, waitFor } from "../../../test-utils/testing-library-utils";
 import Options from "../Options";
+import OrderEntry from "../OrderEntry";
 
 test("update scoop subtotal when scoops selected", async () => {
   // to use Context inside test, need to wrap component with Provider
@@ -65,4 +66,111 @@ test("Update toppings subtotal when toppings selected", async () => {
 
   // toppings subtotal should equal $1.50 after unchecking the Cherries topping
   expect(toppingsSubtotal).toHaveTextContent("1.50");
+});
+
+describe("grand total", () => {
+  test("grand total starts at zero", () => {
+    render(<OrderEntry />);
+
+    // using partial match only works on *byRole screen functions
+    // instead use regex and role to find grand total component
+    const grandTotal = screen.getByRole("heading", { name: /Grand Total: \$/i });
+
+    // check grandTotal to start at zero
+    expect(grandTotal).toHaveTextContent("0.00");
+  });
+
+  test("grand total updates properly if selecting scoop option first", async () => {
+    // render both the scoops and toppings
+    render(<OrderEntry />);
+
+    // get grand total compoent
+    const grandTotal = screen.getByRole("heading", { name: /Grand Total: \$/i });
+
+    // wait for server response with scoops options
+    const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
+    
+    // Increment the vanilla scoop by 1
+    userEvent.clear(vanillaInput); // best to clear input to avoid errors
+    userEvent.type(vanillaInput, "1");
+    
+    // grand total should equal $2.00 after adding 1 to Vanilla scoop
+    expect(grandTotal).toHaveTextContent("2.00");
+
+    // wait for server response with toppings options
+    const cherriesCheckbox = await screen.findByRole("checkbox", { name: "Cherries" });
+
+    // Select the Cherries topping by clicking the checkbox
+    userEvent.click(cherriesCheckbox);
+    expect(cherriesCheckbox).toBeChecked();
+
+    // grand total should equal $3.50 after selecting cherries topping
+    expect(grandTotal).toHaveTextContent("3.50");
+  });
+  
+  test("grand total updates properly if selecting topping option first", async () => {
+    render(<OrderEntry />);
+
+    // get grand total compoent
+    const grandTotal = screen.getByRole("heading", { name: /Grand Total: \$/i });
+
+    // wait for server response with toppings options
+    const hotFudgeCheckbox = await screen.findByRole("checkbox", { name: "Hot fudge" });
+
+    // Select the Hot Fudge topping by clicking the checkbox
+    userEvent.click(hotFudgeCheckbox);
+    expect(hotFudgeCheckbox).toBeChecked();
+
+    // grand total should equal $1.50 after adding Hot Fudge topping
+    expect(grandTotal).toHaveTextContent("1.50");
+
+    // wait for server response with scoops options
+    const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
+
+    // Increment the vanilla scoop by 1
+    userEvent.clear(vanillaInput); // best to clear input to avoid errors
+    userEvent.type(vanillaInput, "1");
+
+    // grand total should equal $3.50 after selecting cherries topping
+    expect(grandTotal).toHaveTextContent("3.50");
+  });
+
+  test("grand total updates properly when removing an option", async () => {
+    render(<OrderEntry />);
+
+    // get grand total compoent
+    const grandTotal = screen.getByRole("heading", { name: /Grand Total: \$/i });
+
+    // wait for response from the server after making a request to the mock server for scoops and toppings
+    const hotFudgeCheckbox = await screen.findByRole("checkbox", { name: "Hot fudge" });
+    const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
+
+    // Select the Hot Fudge topping by clicking the checkbox
+    userEvent.click(hotFudgeCheckbox);
+    expect(hotFudgeCheckbox).toBeChecked();
+
+    // grand total should equal $1.50 after adding Hot fudge topping
+    expect(grandTotal).toHaveTextContent("1.50");
+
+    // Increment the vanilla scoops by 2
+    userEvent.clear(vanillaInput); // best to clear input to avoid errors
+    userEvent.type(vanillaInput, "2");
+
+    // grand total should equal $5.50 after adding 2 vanilla scoops
+    expect(grandTotal).toHaveTextContent("5.50");
+
+    // remove the Vanilla scoop from the order
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, "1");
+
+    // grand total should be equal to $3.50 after removing 1 vanilla scoop
+    expect(grandTotal).toHaveTextContent("3.50");
+
+    // uncheck the Hot fudge topping option
+    userEvent.click(hotFudgeCheckbox);
+    expect(hotFudgeCheckbox).not.toBeChecked();
+
+    // grand total should equal $2.00 after unchecking the Hot fudge topping option
+    expect(grandTotal).toHaveTextContent("2.00");
+  });
 });
