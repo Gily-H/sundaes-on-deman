ebdb211732866922ae6be0e5b25e7bfa838a6514
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-05-21 13:45:34 -0400

    Refactor: order phase test to use case-insensitive regex matches when querying by text. Added assertions for loading screen on confirmation page

--- src/tests/orderPhase.test.js
+++ src/tests/orderPhase.test.js
@@ -13,26 +13,33 @@ test("order phases for happy path", async () => {
   const vanillaInput = await screen.findByRole("spinbutton", { name: "Vanilla" });
   userEvent.clear(vanillaInput);
   userEvent.type(vanillaInput, "1");
-  
+
   // await keyword not necessary if we know that the Vanilla input exists
-  const chocolateInput = screen.getByRole("spinbutton", {name: "Chocolate"});
+  const chocolateInput = screen.getByRole("spinbutton", { name: "Chocolate" });
   userEvent.clear(chocolateInput);
   userEvent.type(chocolateInput, "2");
-  
 
   const cherriesCheckbox = await screen.findByRole("checkbox", { name: "Cherries" });
   userEvent.click(cherriesCheckbox);
 
   // find and click order button - should transition to Summary page
-  const orderButton = screen.getByRole("button", { name: "Order Sundae!" });
+  const orderButton = screen.getByRole("button", { name: /order sundae!/i });
   userEvent.click(orderButton);
 
   // check order summary information based on order
+  const summaryHeader = screen.getByRole("heading", { name: /order summary/i });
+  expect(summaryHeader).toBeInTheDocument();
+
   const scoopsSummary = screen.getByText("Scoops: $", { exact: false });
   expect(scoopsSummary).toHaveTextContent("6.00");
 
   const toppingsSummary = screen.getByText("Toppings: $", { exact: false });
   expect(toppingsSummary).toHaveTextContent("1.50");
+
+  // check list items
+  const optionItems = screen.getAllByRole("listitem");
+  const optionItemsText = optionItems.map((optionItem) => optionItem.textContent);
+  expect(optionItemsText).toEqual(["1 Vanilla", "2 Chocolate", "Cherries"]);
 
   const totalSummary = screen.getByText("Total $", { exact: false });
   expect(totalSummary).toHaveTextContent("7.50");
@@ -40,20 +47,32 @@ test("order phases for happy path", async () => {
   // accept terms and conditions and click button to confirm order
   // should transition to Confirmation page
   const termsAndCondBox = screen.getByRole("checkbox", {
-    name: "I agree to Terms and Conditions",
+    name: /terms and conditions/i,
   });
   userEvent.click(termsAndCondBox);
 
-  const confirmOrderButton = screen.getByRole("button", { name: "Confirm Order" });
+  const confirmOrderButton = screen.getByRole("button", { name: /confirm order/i });
   userEvent.click(confirmOrderButton);
 
+  // initially there will be a loading screen when waiting for axios call to resolve
+  const loadingScreen = screen.getByText(/loading/i);
+  expect(loadingScreen).toBeInTheDocument();
+
+  // async call using axios - wait for response after sending POST request when confirming order
+  // check the order confirmation page information
+  const confirmationHeading = await screen.findByRole("heading", { name: /thank you/i });
+  expect(confirmationHeading).toBeInTheDocument();
+
   // confirm order number on confirmation page
-  // async call using axios - wait for response after sending POST request
   const orderNumber = await screen.findByText("Your order number is ", { exact: false });
   expect(orderNumber).toHaveTextContent("123456789"); // number from mock handler
 
+  // loading screen should not be visible after we get the thank you screen
+  const noLoadingScreen = screen.queryByText(/loading/i);
+  expect(noLoadingScreen).not.toBeInTheDocument();
+
   // click the Create New Order button on the confirmation page
-  const newOrderButton = screen.getByRole("button", { name: "Create new order" });
+  const newOrderButton = screen.getByRole("button", { name: /create new order/i });
   userEvent.click(newOrderButton);
 
   // check that scoop and toppings subtotals have been reset
